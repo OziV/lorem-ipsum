9990cf0efc5aee18577e728508f75be2a7cbc87a
OziV <oz.ziv8@gmail.com>
2022-08-04 14:11:51 +0300

    WIP: adding data

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
 function Home() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handelOnChange = (e) => {
+    const newCount = e.target.value;
+    setCount(newCount);
+  };
+
+  const handelOnSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
     <>
-      <div>Home</div>
+      <section className="section-center">
+        <h3>tired of boring lorem ipsum?</h3>
+        <form className="lorem-form" onSubmit={handelOnSubmit}>
+          <input type="number" name="count" onChange={handelOnChange}></input>
+          <button className="btn">generate</button>
+        </form>
+        <article className="lorem-text">
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
     </>
   );
 }
